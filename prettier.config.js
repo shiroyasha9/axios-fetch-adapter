8c29b567f384d812e003f1dc061aaa618ed3d21f
shiroyasha9 <mubinansari910@gmail.com>
2024-02-09 17:06:40 +0530

    Add prettier

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+};
